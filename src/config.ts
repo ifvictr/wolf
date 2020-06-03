2340240e80d095778014b919a950e1ef3d1435f8
Victor Truong <victor@victortruong.com>
2020-06-02 18:40:35 -0700

    Remove botToken from config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 interface WolfConfig {
     databaseUrl: string
     port: number | string
-    botToken: string
     signingSecret: string
     stateSecret: string
     clientId: string
@@ -13,7 +12,6 @@ const config: WolfConfig = {
     databaseUrl: process.env.DATABASE_URL || process.env.MONGODB_URI || '',
     port: process.env.PORT || 3000,
     // Slack-specific config
-    botToken: process.env.SLACK_CLIENT_BOT_TOKEN || '',
     signingSecret: process.env.SLACK_CLIENT_SIGNING_SECRET || '',
     stateSecret: process.env.SLACK_STATE_SECRET || '',
     clientId: process.env.SLACK_CLIENT_ID || '',
